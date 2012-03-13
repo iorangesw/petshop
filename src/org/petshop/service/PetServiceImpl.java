a675913a9b488c40a311bc100625a584636c4d89
teixeiragpt <teixeiragpt@gmail.com>
2012-03-13 13:42:25 -0300

    Update src/org/petshop/service/PetServiceImpl.java

--- src/org/petshop/service/PetServiceImpl.java
+++ src/org/petshop/service/PetServiceImpl.java
@@ -5,75 +5,42 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import org.petshop.dominio.Pet;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
+@Service("petService")
 public class PetServiceImpl implements PetService {
 
 	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("petshop");
 
+	@PersistenceContext
+	private EntityManager entityManager;
+
 	@Override
 	public void save(Pet pet) {
-		EntityManager em = emf.createEntityManager();
-		try {
-			em.getTransaction().begin();
-			em.persist(pet);
-			em.getTransaction().commit();
-		} catch (Exception e) {
-			em.getTransaction().rollback();
-		} finally {
-			em.close();
-		}
+		entityManager.merge(pet);
 	}
 
 	@Override
 	public List<Pet> getAllPets() {
-		EntityManager em = emf.createEntityManager();
-		List<Pet> pets = null;
-		try {
-			em.getTransaction().begin();
-			Query query = em.createQuery("SELECT pet FROM Pet pet");
-			pets = query.getResultList();
-			em.getTransaction().commit();
-		} catch (Exception e) {
-			em.getTransaction().rollback();
-		} finally {
-			em.close();
-		}
-		return pets;
+		Query query = entityManager.createQuery("SELECT pet FROM Pet pet");
+		return query.getResultList();
 	}
-	
+
 	@Override
 	public Pet searchPet(Long id) {
-		EntityManager em = emf.createEntityManager();
-		Pet pet = null;
-		try {
-			em.getTransaction().begin();
-			Query query = em.createQuery("SELECT pet FROM Pet pet WHERE pet.id = "+id);
-			pet = (Pet) query.getSingleResult();
-			em.getTransaction().commit();
-		} catch (Exception e) {
-			em.getTransaction().rollback();
-		} finally {
-			em.close();
-		}
-		return pet;
+		Query query = entityManager.createQuery("SELECT pet FROM Pet pet WHERE pet.id = "+id);
+		return (Pet) query.getSingleResult();
 	}
 
 	@Override
 	public void removePet(Pet pet) {
-		EntityManager em = emf.createEntityManager();
-		try {
-			em.getTransaction().begin();
-			Pet petAble = em.find(Pet.class, pet.getId());
-			em.remove(petAble);
-			em.getTransaction().commit();
-		} catch (Exception e) {
-			em.getTransaction().rollback();
-		} finally {
-			em.close();
-		}
+		Pet petAble = entityManager.find(Pet.class, pet.getId());
+		entityManager.remove(petAble);
 	}
-
 }
